abb8ce4a06d0599a6dfe05d1ac0f21153d7a63dd
sigeo944 <sigeo944@learner.dvusd.org>
2024-01-22 09:28:46 -0700

    Pokedex Update

--- Pokedex/Pokedex/Form1.cs
+++ Pokedex/Pokedex/Form1.cs
@@ -17,15 +17,15 @@ namespace Pokedex
     }
     struct Pokemon
     {
-        string Name;
-        string Type;
-        int Level;
-        Attacks attackType;
-        int HP;
-        int Exp;
-        bool Legendary;
-        bool Shiny;
-        int Gen;
+        public string Name;
+        public string Type;
+        public int Level;
+        public Attacks attackType;
+        public int HP;
+        public int Exp;
+        public bool Legendary;
+        public bool Shiny;
+        public int Gen;
     }
     public partial class Form1 : Form
     {
@@ -40,8 +40,30 @@ namespace Pokedex
             {
                 StreamReader inFile = new StreamReader("Pokemon.txt");
                 string S = inFile.ReadToEnd();
+                ReadPokemon(S);
                 inFile.Close();
             }
+        }
+
+        private void ReadPokemon(string s)
+        {
+            Pokemon p = new Pokemon();
+            string[] fields = s.Split('|');
+            p.Name = fields[0];
+            p.Type = fields[1];
+            p.Level = int.Parse(fields[2]);
+            p.attackType = (Attacks)Enum.Parse(typeof(Attacks), fields[3]);
+            p.HP = int.Parse(fields[4]);
+            p.Exp = int.Parse(fields[5]);
+            if (fields[6] == "True")
+                p.Legendary = true;
+            else
+                p.Legendary = false;
+            if (fields[7] == "True")
+                p.Shiny = true;
+            else
+                p.Shiny = false;
+            p.Gen = int.Parse(fields[8]);
         }
 
         private void saveB_Click(object sender, EventArgs e)
@@ -53,8 +75,6 @@ namespace Pokedex
             debugTB.Text += lvlUD.Value;
             debugTB.Text += "|";
             debugTB.Text += atDD.Text;
-            debugTB.Text += "|";
-            debugTB.Text += nameTB.Text;
             debugTB.Text += "|";
             debugTB.Text += hpTB.Text;
             debugTB.Text += "|";
